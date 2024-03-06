f795bb8b5693225552e18969c2f2f11384d35f4d
Ankur Gupta <ankurnecessary@gmail.com>
2024-03-07 03:42:59 +1030

    Added task delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.taskId !== id),
+      };
+    });
+  }
 
   function handleSelectProject(id) {
     setProjectState((prevState) => {
